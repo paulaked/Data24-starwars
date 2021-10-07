d484249ded8a5ddc45d97be37f7a77db55187c80
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-10-07 14:29:37 +0100

    UPDATE: __main__ file to test printing

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -1,3 +1,9 @@
 
+
+test_text= "\"Differences of habit and language are nothing at all if our " \
+           "aims are identical and our hearts are open\" \n Albus Dumbledore, " \
+           "Harry Potter and the Goblet of Fire, Chapter 37 "
+
+
 if __name__ == '__main__':
-    pass  # Replace this with code to run your app
+    print(test_text)
