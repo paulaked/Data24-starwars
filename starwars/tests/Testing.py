3e9c7c44200c882a825a0526958a4a2e3ad770e8
Tia <tiaedmead@Tias-MacBook-Pro.local>
2021-10-07 23:00:04 +0100

    Response Body Element Check
    Checks that the response body element "name", in starships, is equal to "Millennium Falcon"

--- starwars/tests/Testing.py
+++ starwars/tests/Testing.py
@@ -5,3 +5,8 @@ def test_get_response_for_website_check_status_code_equals_200():
     response = requests.get("https://swapi.dev")
     assert response.status_code == 200
 
+
+def test_get_starship_check_name_equals_millennium_falcon_():
+    response = requests.get("https://swapi.dev/api/starships/10/")
+    response_body = response.json()
+    assert response_body["name"] == "Millennium Falcon"
