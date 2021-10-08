731c772ebb5e958bc77f3576ecba61257625bc16
Tia <tiaedmead@Tias-MacBook-Pro.local>
2021-10-08 12:28:11 +0100

    Update Testing.py
    Changed the links and used the variable names from config_manager.py

--- starwars/tests/Testing.py
+++ starwars/tests/Testing.py
@@ -1,12 +1,13 @@
+import starwars.config_manager as conf
 import requests as requests
 
 
 def test_get_response_for_website_check_status_code_equals_200():
-    response = requests.get("https://swapi.dev")
+    response = requests.get(conf.SWAPI_URL)
     assert response.status_code == 200
 
 
 def test_get_starship_check_name_equals_millennium_falcon_():
-    response = requests.get("https://swapi.dev/api/starships/10/")
+    response = requests.get(conf.SWAPI_STARSHIPS_MILLENNIUM_FALCON_URL)
     response_body = response.json()
     assert response_body["name"] == "Millennium Falcon"
