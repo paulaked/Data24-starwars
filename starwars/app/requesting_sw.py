9f31ddc9a0caf4dccc85d3183dc8b0490f50d4e3
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-10-08 15:24:30 +0100

    UPDATE: requesting_sw.py by adding request function

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,6 +1,24 @@
 import starwars.config_manager as conf
 import requests
+import json
 
-sw = requests.get(conf.SWAPI_URL)
 
-print(sw)
+# This connect with conf.SWAPI_URL by requests package
+# Catches 2 errors: ConnectionError & JSONDecodeError
+# Returns False if fail
+# Returns JSON file is success
+def get_request():
+    starwars_Data = None
+    try:
+        # convert to JSON file
+        starwars_Data = requests.get(f'{conf.SWAPI_URL}/api/starships', headers={'content-type': 'application/json'}).json()
+    except requests.exceptions.ConnectionError as CE:
+        print(CE)
+    except json.decoder.JSONDecodeError as JSONError:
+        print(f"Decoder problem (check the URL): {JSONError}")
+    if starwars_Data:
+        return starwars_Data
+    else:
+        return False
+
+
