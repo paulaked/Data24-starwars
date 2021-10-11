af05b117749bdd54c1fb37ad6e69571ec64a8f5a
Tia <tiaedmead@Tias-MacBook-Pro.local>
2021-10-11 10:42:51 +0100

    Update config_manager.py
    Added a url variable for the people section of the API

--- starwars/config_manager.py
+++ starwars/config_manager.py
@@ -8,4 +8,5 @@ _config.read(os.path.join(PROJECT_ROOT_DIR, 'config.ini'))
 
 SWAPI_URL = _config['default']['url']
 SWAPI_STARSHIPS_MILLENNIUM_FALCON_URL = _config['default']['url2']
+SWAPI_PEOPLE_URL = _config['default']['url3']
 
