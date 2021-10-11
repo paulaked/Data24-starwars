bd255707fe06a70d1e102879c4975cbc404788c1
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-10-11 20:26:21 +0100

    UPDATE: test file

--- starwars/tests/test.py
+++ starwars/tests/test.py
@@ -1,6 +1,8 @@
 import pytest
 import starwars.config_manager as conf
 import requests as rq
+import starwars.app.starshipsAPI as sapi
+
 
 
 REQ = rq.get(conf.SWAPI_URL, headers={'content-type': 'application/json'})
@@ -36,13 +38,13 @@ def test_get_request():
 # This test checks the names of starships
 def test_get_starships():
     count = 36
-    list_length = len(star.get_starships(JSON['starships']))
+    list_length = len(sapi.get_starships(JSON['starships']))
     assert list_length == count
 
 
 # This test checks the URLs of pilots
 def test_get_pilots_URLS():
-    starships = REQ["starships"]
+    starships = JSON["starships"]
     pilot_of_T1_URL = starships["pilots"]
     pilot_test_URL = "https//: ..."
     assert pilot_of_T1_URL == pilot_test_URL
