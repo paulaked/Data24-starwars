75a9715f6f4cd7c4c65bdff6daa014649fff7a44
Tia <tiaedmead@Tias-MacBook-Pro.local>
2021-10-11 12:12:28 +0100

    requesting_starships_pilot.py
    Changed the name of the function to avoid confusion with the starships name function

--- starwars/app/requesting_starships_pilot.py
+++ starwars/app/requesting_starships_pilot.py
@@ -11,18 +11,18 @@ where the 'next' value is 'null'
 """
 
 
-def go_through_starships(api_link):
+def go_through_starships_name_pilots(api_link):
     response = requests.get(api_link)
     starships_result = response.json()
     for starship in starships_result['results']:
         yield starship['name'], starship['pilots']
     if 'next' in starships_result and starships_result['next'] is not None:
-        next_page = go_through_starships(starships_result['next'])
+        next_page = go_through_starships_name_pilots(starships_result['next'])
         for page in next_page:
             yield page
 
 
-list_of_starships = go_through_starships(conf.SWAPI_STARSHIPS_URL)
+list_of_starships = go_through_starships_name_pilots(conf.SWAPI_STARSHIPS_URL)
 for ship in list_of_starships:
     print(ship)
 
