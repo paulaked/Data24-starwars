50fd43fb581219c4e21b0360cbbc506cc9443b4a
rfabijan <91061895+rfabijan@users.noreply.github.com>
2021-10-11 11:14:33 +0100

    ADD: new test file ---- DELETE: code from tests/__init__

---
+++ starwars/tests/test.py
@@ -0,0 +1,51 @@
+import pytest
+import starwars.config_manager as conf
+import requests as rq
+
+
+REQ = rq.get(conf.SWAPI_URL, headers={'content-type': 'application/json'})
+JSON = REQ.json()
+
+'''
+=====================================REQUESTS TESTS=========================================== 
+'''
+
+
+# This test checks the URL
+def test_get_URL():
+    assert conf.SWAPI_URL == "https://swapi.dev//api"
+
+
+# This test checks the request status code (must be 200)
+def test_check_request_code():
+    assert REQ.status_code == 200
+
+
+# This test checks if request returns an appropriate format
+def test_get_request():
+    test_dict = {'name': 'Tesla', 'engine': 'electric'}
+    assert type(JSON) == type(test_dict)
+
+
+
+
+'''
+=====================================DATA EXTRACTION TESTS=========================================== 
+'''
+
+# This test checks the names of starships
+def test_get_starships():
+    count = 36
+    list_length = len(star.get_starships(JSON['starships']))
+    assert list_length == count
+
+
+# This test checks the URLs of pilots
+def test_get_pilots_URLS():
+    starships = REQ["starships"]
+    pilot_of_T1_URL = starships["pilots"]
+    pilot_test_URL = "https//: ..."
+    assert pilot_of_T1_URL == pilot_test_URL
+
+
+
