f40d651025052ccd5ccaa5e23d99cdc2ce1be1a2
LXSeeamber <lseeamber@spartaglobal.com>
2021-10-12 01:07:08 +0100

    pre final

--- starwars/tests/program.py
+++ starwars/tests/program.py
@@ -1,23 +1,18 @@
 from file1 import *
 
-# Database = 'StarWars'
-# Collection = 'starships'
-#
-# create_collection(Database, Collection)
-#
-# for i in range(100):
-#     try:
-#
-#         starship = get_info(i, 'name', 'starships')
-#         pilot = get_pilot_object_ids(i)
-#         add_starship_document(Database, Collection, starship, pilot)
-#
-#     except Exception:
-#
-#         pass
+Database = 'StarWars'
+Collection = 'starships'
 
-client = pymongo.MongoClient()
-db = client['StarWars']
-db.get_collection('starships').find(
-        {'starship': "Millennium Falcon"}
-        )
+create_collection(Database, Collection)
+
+for i in range(100):
+    try:
+
+        starship = get_info(i, 'name', 'starships')
+        pilot = get_pilot_object_ids(i)
+        add_starship_document(Database, Collection, starship, pilot)
+
+    except Exception:
+
+        pass
+
